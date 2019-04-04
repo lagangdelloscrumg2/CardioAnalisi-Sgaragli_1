f30af8ef0e5fa87b21a7876a1faaf261616f7d95
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-04 10:14:20 +0200

    Add project files.

---
+++ CardioLibrary/DataCardio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardioLibrary
+{
+    public class DataCardio
+    {
+        public static int Battiti(int età)
+        {
+            
+            int frequenza = 220;
+            int battito = frequenza - età;
+
+            return battito;
+        }
+    }
+}
---
+++ UnitTestProject1/DataCardio.Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CardioLibrary;
+
+namespace DataCardio.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int età = 21;
+
+            int battitofiero = 199;
+
+            int aspettativa=CardioLibrary.DataCardio.Battiti(età);
+
+            Assert.AreEqual(battitofiero, aspettativa);
+
+        }
+    }
+}
